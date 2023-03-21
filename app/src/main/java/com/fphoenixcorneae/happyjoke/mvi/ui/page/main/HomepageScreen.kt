92a42ca91abcd07dfd34be0083d61ffeb8c19b7f
wangkz <wangkz@infore.com>
2023-03-21 14:03:32 +0800

    feat(首页): 短视频播放器

--- app/src/main/java/com/fphoenixcorneae/happyjoke/mvi/ui/page/main/HomepageScreen.kt
+++ app/src/main/java/com/fphoenixcorneae/happyjoke/mvi/ui/page/main/HomepageScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -16,6 +17,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
@@ -32,6 +34,7 @@ import coil.transform.CircleCropTransformation
 import com.fphoenixcorneae.happyjoke.R
 import com.fphoenixcorneae.happyjoke.ext.noRippleClickable
 import com.fphoenixcorneae.happyjoke.mvi.ui.theme.GreyLine
+import com.fphoenixcorneae.happyjoke.mvi.ui.widget.ShortVideoPlayer
 import com.fphoenixcorneae.happyjoke.mvi.ui.widget.SystemUiScaffold
 
 /**
@@ -100,7 +103,7 @@ fun HomepageScreen(
                 )
             }
             Divider(color = GreyLine, thickness = 0.5.dp)
-            LazyColumn(modifier = Modifier.fillMaxSize()) {
+            LazyColumn(modifier = Modifier.fillMaxSize(), contentPadding = PaddingValues(bottom = 60.dp)) {
                 items(count = 10) {
                     HomepageItem()
                 }
@@ -132,7 +135,7 @@ fun HomepageItem() {
                         .build(),
                     contentDescription = null,
                     modifier = Modifier
-                        .size(48.dp)
+                        .size(36.dp)
                         .constrainAs(avatar) {
                             start.linkTo(parent.start, margin = 16.dp)
                             top.linkTo(parent.top)
@@ -141,21 +144,29 @@ fun HomepageItem() {
                 )
                 Text(
                     text = "偷蜂蜜的跳跳虎",
-                    style = TextStyle(color = Color.Black, fontSize = 15.sp),
+                    style = TextStyle(color = Color.Black, fontSize = 14.sp),
                     modifier = Modifier.constrainAs(name) {
-                        start.linkTo(avatar.end, margin = 16.dp)
+                        start.linkTo(avatar.end, margin = 8.dp)
                         top.linkTo(avatar.top)
                     },
                 )
                 Text(
                     text = "偷蜂蜜的跳跳虎",
-                    style = TextStyle(color = Color.Gray, fontSize = 14.sp),
+                    style = TextStyle(color = Color.Gray, fontSize = 13.sp),
                     modifier = Modifier.constrainAs(desc) {
                         start.linkTo(name.start)
-                        top.linkTo(name.bottom, margin = 4.dp)
+                        top.linkTo(name.bottom, margin = 2.dp)
                     },
                 )
             }
+            ShortVideoPlayer(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(120.dp)
+                    .padding(horizontal = 16.dp)
+                    .clip(shape = RoundedCornerShape(8.dp)),
+                videoUrl = "https://prod-streaming-video-msn-com.akamaized.net/a8c412fa-f696-4ff2-9c76-e8ed9cdffe0f/604a87fc-e7bc-463e-8d56-cde7e661d690.mp4"
+            )
         }
         Divider(color = GreyLine, thickness = 10.dp)
     }
---
+++ app/src/main/java/com/fphoenixcorneae/happyjoke/mvi/ui/widget/ShortVideoPlayer.kt
@@ -0,0 +1,139 @@
+package com.fphoenixcorneae.happyjoke.mvi.ui.widget
+
+import android.net.Uri
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import com.fphoenixcorneae.happyjoke.R
+import com.fphoenixcorneae.happyjoke.ext.noRippleClickable
+import com.google.android.exoplayer2.C
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.source.dash.DashMediaSource
+import com.google.android.exoplayer2.source.hls.HlsMediaSource
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.ui.AspectRatioFrameLayout
+import com.google.android.exoplayer2.ui.StyledPlayerView
+import com.google.android.exoplayer2.upstream.DefaultDataSource
+import com.google.android.exoplayer2.util.Util
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+/**
+ * @desc：短视频播放器
+ * @date：2023/03/20 17:48
+ */
+@Composable
+fun ShortVideoPlayer(
+    modifier: Modifier,
+    videoUrl: String?,
+) {
+    Box(modifier = modifier) {
+        val context = LocalContext.current
+        val scope = rememberCoroutineScope()
+
+        val exoPlayer = remember {
+            ExoPlayer.Builder(context)
+                .setTrackSelector(
+                    DefaultTrackSelector(context).apply {
+                        setParameters(
+                            buildUponParameters().setMaxVideoSizeSd()
+                                .setAllowAudioMixedMimeTypeAdaptiveness(true)
+                        )
+                    }
+                )
+                .build()
+                .apply {
+                    videoScalingMode = C.VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING
+                    repeatMode = Player.REPEAT_MODE_ONE
+                    val videoUri = Uri.parse(videoUrl)
+                    val dataSourceFactory = DefaultDataSource.Factory(context)
+                    val mediaSource = when (Util.inferContentType(videoUri)) {
+                        C.CONTENT_TYPE_DASH -> DashMediaSource.Factory(dataSourceFactory)
+                            .createMediaSource(MediaItem.fromUri(videoUri))
+                        C.CONTENT_TYPE_HLS -> HlsMediaSource.Factory(dataSourceFactory)
+                            .createMediaSource(MediaItem.fromUri(videoUri))
+                        else -> ProgressiveMediaSource.Factory(dataSourceFactory)
+                            .createMediaSource(MediaItem.fromUri(videoUri))
+                    }
+                    addMediaSource(mediaSource)
+                    prepare()
+                }
+        }
+        DisposableEffect(
+            Box(modifier = Modifier.fillMaxSize()) {
+                var visible by remember { mutableStateOf(true) }
+                AndroidView(
+                    factory = {
+                        StyledPlayerView(context).apply {
+                            hideController()
+                            useController = false
+                            resizeMode = AspectRatioFrameLayout.RESIZE_MODE_ZOOM
+                            player = exoPlayer
+                            layoutParams = FrameLayout.LayoutParams(
+                                ViewGroup.LayoutParams.MATCH_PARENT,
+                                ViewGroup.LayoutParams.MATCH_PARENT
+                            )
+                        }
+                    },
+                    modifier = Modifier.noRippleClickable {
+                        visible = !visible
+                    }
+                )
+                var isPlaying by remember { mutableStateOf(exoPlayer.isPlaying) }
+                AnimatedVisibility(
+                    visible = visible,
+                    enter = fadeIn(),
+                    exit = fadeOut(),
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                        .size(48.dp)
+                        .noRippleClickable {
+                            scope.launch {
+                                delay(500)
+                                isPlaying = if (isPlaying) {
+                                    exoPlayer.pause()
+                                    false
+                                } else {
+                                    exoPlayer.play()
+                                    true
+                                }
+                            }
+                        },
+                ) {
+                    Icon(
+                        painter = painterResource(id = if (isPlaying) R.mipmap.ic_pause else R.mipmap.ic_play),
+                        contentDescription = null,
+                        tint = Color.White,
+                    )
+                    if (isPlaying) {
+                        LaunchedEffect(Unit) {
+                            delay(5000)
+                            visible = false
+                        }
+                    }
+                }
+            }
+        ) {
+            onDispose {
+                exoPlayer.release()
+            }
+        }
+    }
+}
