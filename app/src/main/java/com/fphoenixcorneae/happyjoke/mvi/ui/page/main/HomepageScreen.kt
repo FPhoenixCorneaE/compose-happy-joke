962fa35118a951e5d41d42a2a1d362fe5430ac98
wangkz <wangkz@infore.com>
2023-03-21 17:47:09 +0800

    feat(首页): compose ui

--- app/src/main/java/com/fphoenixcorneae/happyjoke/mvi/ui/page/main/HomepageScreen.kt
+++ app/src/main/java/com/fphoenixcorneae/happyjoke/mvi/ui/page/main/HomepageScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,6 +22,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -125,8 +127,12 @@ fun HomepageItem() {
             modifier = Modifier
                 .background(color = MaterialTheme.colorScheme.background)
         ) {
-            ConstraintLayout(modifier = Modifier.height(60.dp)) {
-                val (avatar, name, desc) = createRefs()
+            ConstraintLayout(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(60.dp)
+            ) {
+                val (avatar, name, desc, more) = createRefs()
                 AsyncImage(
                     model = ImageRequest.Builder(context)
                         .data(ColorDrawable(Color.Gray.toArgb()))
@@ -158,15 +164,98 @@ fun HomepageItem() {
                         top.linkTo(name.bottom, margin = 2.dp)
                     },
                 )
+                Icon(
+                    painter = painterResource(id = R.mipmap.ic_more),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(24.dp)
+                        .constrainAs(more) {
+                            top.linkTo(parent.top)
+                            bottom.linkTo(parent.bottom)
+                            end.linkTo(parent.end, margin = 16.dp)
+                        },
+                )
             }
+            Text(
+                text = "愤怒的备胎",
+                style = TextStyle(color = Color.Black, fontSize = 14.sp),
+                modifier = Modifier.padding(start = 16.dp, top = 8.dp),
+            )
             ShortVideoPlayer(
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(120.dp)
-                    .padding(horizontal = 16.dp)
+                    .padding(start = 16.dp, top = 8.dp, end = 16.dp)
                     .clip(shape = RoundedCornerShape(8.dp)),
                 videoUrl = "https://prod-streaming-video-msn-com.akamaized.net/a8c412fa-f696-4ff2-9c76-e8ed9cdffe0f/604a87fc-e7bc-463e-8d56-cde7e661d690.mp4"
             )
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(40.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Row(
+                    modifier = Modifier.weight(1f),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.mipmap.ic_like),
+                        contentDescription = null,
+                        modifier = Modifier.size(24.dp)
+                    )
+                    Text(
+                        text = "1",
+                        style = TextStyle(color = Color.Black, fontSize = 10.sp),
+                    )
+                }
+                Row(
+                    modifier = Modifier.weight(1f),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.mipmap.ic_tread),
+                        contentDescription = null,
+                        modifier = Modifier.size(24.dp)
+                    )
+                    Text(
+                        text = "88",
+                        style = TextStyle(color = Color.Black, fontSize = 10.sp),
+                    )
+                }
+                Row(
+                    modifier = Modifier.weight(1f),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.mipmap.ic_comment),
+                        contentDescription = null,
+                        modifier = Modifier.size(24.dp)
+                    )
+                    Text(
+                        text = "20",
+                        style = TextStyle(color = Color.Black, fontSize = 10.sp),
+                    )
+                }
+                Row(
+                    modifier = Modifier.weight(1f),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.mipmap.ic_share),
+                        contentDescription = null,
+                        modifier = Modifier.size(24.dp)
+                    )
+                    Text(
+                        text = "38",
+                        style = TextStyle(color = Color.Black, fontSize = 10.sp),
+                    )
+                }
+            }
         }
         Divider(color = GreyLine, thickness = 10.dp)
     }
